b7fc0bcc398f2aefd096bfe1c70d3265422d7a72
jamerrq <jamerrq@gmail.com>
2025-08-02 18:04:23 -0500

    nikki: backup

--- dev/scripts/bash/test-notifications.sh
+++ dev/scripts/bash/test-notifications.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-notify-send -u low "low" -t 0
+notify-send -u low "low" "low" -t 0
 sleep 1
-notify-send -u normal "normal" -t 0
+notify-send -u normal "normal" "normal" -t 0
 sleep 1
-notify-send -u critical "critical" -t 0
+notify-send -u critical "critical" "critical" -t 0
 sleep 1
-notify-send -a "kitty" "kitty" -t 0
+notify-send -a "kitty" "kitty" "kitty" -t 0
 sleep 1
-notify-send -a "spotify" "spotify" -t 0
+notify-send -a "Spotify" "spotify" "spotify" -t 0
