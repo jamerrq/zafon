460def0a31b1ca2feee53f294cedf0e9a8e982fb
Jamer Rebolledo <jamerrq@gmail.com>
2026-04-04 13:33:34 -0500

    nvim: update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- nikki customizations
-vim.opt.clipboard = "unnamedplus" -- sync vim clipboard with the system's one
-vim.opt.colorcolumn = "80,120" -- ruler
+-- customizations
+vim.opt.clipboard = "unnamedplus"                         -- sync clipboard
+vim.opt.colorcolumn = "80,120"                            -- ruler
 vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#AACDDC" }) -- ruler's color
+vim.opt.relativenumber = true                             -- relative numbers
